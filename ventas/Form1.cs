30d5109ca89f71de7bd359218d660278ea3dba6e
Danielihb <90307574+Danielihb@users.noreply.github.com>
2021-09-17 04:52:53 +0200

    frase motivadora
    frase motivadora

--- ventas/Form1.cs
+++ ventas/Form1.cs
@@ -19,7 +19,7 @@ namespace ventas
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hola Mundo");
+            MessageBox.Show("Debes hacer las cosas que crees que no puedes hacer");
         }
     }
 }
